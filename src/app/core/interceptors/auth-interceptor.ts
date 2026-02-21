51eee6f090255a21d51ae8e83c0085ff1a7fc0d1
Annihilux <annihilux20@gmail.com>
2026-02-21 16:36:45 +0100

    AuthInterceptor añadido (JWT en Authorization)

--- src/app/core/interceptors/auth-interceptor.ts
+++ src/app/core/interceptors/auth-interceptor.ts
@@ -1,5 +1,22 @@
 import { HttpInterceptorFn } from '@angular/common/http';
+import { inject } from '@angular/core';
+import { AuthService } from '../services/auth';
 
 export const authInterceptor: HttpInterceptorFn = (req, next) => {
-  return next(req);
+  const auth = inject(AuthService);
+
+  // JWT actual (si existe)
+  const accessToken = auth.session()?.access_token;
+
+  if (!accessToken) {
+    return next(req);
+  }
+
+  const authReq = req.clone({
+    setHeaders: {
+      Authorization: `Bearer ${accessToken}`
+    }
+  });
+
+  return next(authReq);
 };
