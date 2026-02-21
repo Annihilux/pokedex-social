9330162880d2ea4400cc3695ef4e033729f221e5
Annihilux <annihilux20@gmail.com>
2026-02-21 13:04:52 +0100

    Configuración inicial Supabase y enlazado con el proyecto

---
+++ src/environments/environment.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  production: false,
+  supabaseUrl: 'https://bthctderavgqgqcxhbdm.supabase.co',
+  supabaseKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJ0aGN0ZGVyYXZncWdxY3hoYmRtIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzE2MjQ4OTgsImV4cCI6MjA4NzIwMDg5OH0.U8ZnPCfv9tchb8xAIug9RedGBLyEbaGlfJi0NWRwWxU'
+};
