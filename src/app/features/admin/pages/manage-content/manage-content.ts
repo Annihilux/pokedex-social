e157aa5987ad6ba5c276c95a8dddc65bc9045ab2
Annihilux <annihilux20@gmail.com>
2026-02-21 12:31:13 +0100

    Estructura base final del proyecto creada, ahora empieza lo chungo

---
+++ src/app/features/admin/pages/manage-content/manage-content.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-manage-content',
+  imports: [],
+  templateUrl: './manage-content.html',
+  styleUrl: './manage-content.scss',
+})
+export class ManageContent {
+
+}
