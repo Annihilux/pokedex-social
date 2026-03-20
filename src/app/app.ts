34903829743bf408c474b73aebac54c4caa90b06
Annihilux <annihilux20@gmail.com>
2026-03-21 00:08:10 +0100

    chore: remove unused loader and dead app code

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,6 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { NavbarComponent } from "./shared/components/navbar/navbar";
-
-import { ApplicationConfig } from '@angular/core';
-import { provideRouter } from '@angular/router';
-import { provideHttpClient } from '@angular/common/http';
-import { routes } from './app.routes';
+import { NavbarComponent } from './shared/components/navbar/navbar';
 
 
 @Component({
@@ -18,10 +13,3 @@ import { routes } from './app.routes';
 export class App {
   protected readonly title = signal('pokedex-social');
 }
-
-export const appConfig: ApplicationConfig = {
-  providers: [
-    provideRouter(routes),
-    provideHttpClient()
-  ]
-};
